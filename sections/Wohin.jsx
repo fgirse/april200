57429735e825621a191032fa7a20091aab7daa47
fgirse <fgirse@bluewin.ch>
2023-04-12 04:31:23 +0200

    29er commit

--- sections/Wohin.jsx
+++ sections/Wohin.jsx
@@ -1,18 +1,22 @@
 "use client";
 import Modale01 from '../components/Modale/Modale01'
+import Modale02 from '../components/Modale/Modale02'
 import Image from'next/image'
 
 const Wohin = () => 
-<section id="section-wohin" className="bg-wohin bg-no-repeat w-full h-[69vh] text-center text-yellow-500 ">
+<section id="section-wohin" className="mb-[20vh] bg-wohin bg-no-repeat w-full h-[69vh] text-center text-yellow-500 ">
             
     <h1 className="mt-[12vh] text-yellow-500 headingA text-[3rem] md:text-[6rem] ">Wohin ?</h1>
     <div className='flex flex-col justify-center items-center'>
-        <h1 className='mb-[3vh] text-[1.33rem] text-white md:w-[33vw                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     ] md:text-3xl'>Sie finden die genaue Lage unseres Rettungsankers in der Kartenillustration der Altstadt Freiburg oder ganz profesionelle in Open-Street-Map</h1>
-  
-        
+        <h1 className='mb-[3vh] text-[1.33rem] text-white md:w-[33vw] md:text-3xl'>Sie finden die genaue Lage unseres Rettungsankers in der Kartenillustration der Altstadt Freiburg oder ganz profesionelle in Open-Street-Map</h1>
         <Modale01/>
-        <div className='mb-[3vh] w-[66vw]  bg-slate-800 rounded-2xl border flex flex-col justify-center items-center lg:flex-row'>
-        <Image className='mt-[3vh]  ' src="/VAGLogo.png" width="200" height="160" alt="VAG Logo"/>
+
+
+
+
+        <Modale02/>
+        <div className='mb-[5vh] w-[66vw]  bg-slate-800 rounded-2xl border flex flex-col justify-center items-center lg:flex-row'>
+        <Image className='mt-[5vh]  ' src="/VAGLogo.png" width="200" height="160" alt="VAG Logo"/>
         <h1 className='text-orange text-[2rem] '>Haltestelle</h1>
 
 
