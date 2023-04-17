dbb1055a3090814826163cd0ee9945b0ed2d47fb
fgirse <fgirse@bluewin.ch>
2023-04-17 11:17:05 +0200

    39er commit

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -14,13 +14,13 @@ const Hero = () => (
       viewport={{ once: false, amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto flex flex-col`}
     >
-   <div className='mx-auto mt-[3vh] bg-gray-900/30 w-[90vw] h-full flex flex-col items-center justify-center'>
-      <Image src="/LogoNeu.png" width="160" height="260"  alt="LogoNeu" className="lg:hidden "/>
+   <div className='mx-auto mt-[3vh] py-4 bg-gray-900/30 w-[90vw] h-[80vh] flex flex-col items-center justify-between'>
+      <Image src="/LogoNeu.png" width="160" height="260"  alt="LogoNeu" className="md:mt-[0vh] md:w-[45vw] md:h-[15vh] lg:hidden "/>
       
       <div className="flex justify-center items-center flex-col relative z-10 ">
         <motion.h1
           variants={textVariant(1.1)}
-          className="mt-[37vh] mb-[3vh] headingE text-yellow-50 text-[9.66rem] md:mt-[45vw] lg:-mt-[5vh] lg:mb-[1vh]  lg:text-[19rem] 2xl:text-[23rem]"
+          className="mt-[37vh] mb-[3vh] headingE text-yellow-50 text-[9.66rem] md:mt-[6vh] md:text-[13.33rem] lg:-mt-[5vh] lg:mb-[2vh]  lg:text-[19rem] 2xl:text-[23rem]"
         >
           die
         </motion.h1>
@@ -28,10 +28,10 @@ const Hero = () => (
           variants={textVariant(1.2)}         
           className="mt[3-vh] flex flex-row justify-center items-center"
         >
-          <motion.h1 className="-mt-[12vh] headingA lg:-mt-[22vh] text-[2.9rem] md:text-[9rem] lg:text-[11rem] text-red-700 2xl:headingF 2xl:text-[14rem]">
+          <motion.h1 className="-mt-[12vh] headingA lg:-mt-[18vh] text-[2.9rem] md:text-[7rem] lg:text-[11rem] text-red-700 2xl:headingF 2xl:text-[14rem]">
             kiez
           </motion.h1>
-          <motion.h1 className="-mt-[12vh] headingA lg:-mt-[22vh] text-[2.9rem] md:text-[9rem] lg:text-[11rem] text-red-700 2xl:headingF 2xl:text-[14rem]">
+          <motion.h1 className="-mt-[12vh] headingA lg:-mt-[18vh] text-[2.9rem] md:text-[7rem] lg:text-[11rem] text-red-700 2xl:headingF 2xl:text-[14rem]">
             kneipe
           </motion.h1>
         </motion.div>
