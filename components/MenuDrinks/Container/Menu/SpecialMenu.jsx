a6e945bf65710b218fdeb5ea16640b8b5e28a1fe
fgirse <fgirse@bluewin.ch>
2023-04-28 01:36:40 +0200

    6er commit

--- components/MenuDrinks/Container/Menu/SpecialMenu.jsx
+++ components/MenuDrinks/Container/Menu/SpecialMenu.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import {  MenuItem, SubHeading } from '../../../MenuDrinks';
-import { data, images  } from '../../../../Data';
+import { data, images  } from '../../../../data/'
 import './SpecialMenu.css';
 
 const SpecialMenu = () => (
